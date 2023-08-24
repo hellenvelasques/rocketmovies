996c52a7772bcd3f9c949610c8410b67ee14c4ce
hellenvelasques <hgv92jc@gmail.com>
2023-08-24 09:56:36 -0300

    feat: criada a página de detalhes e ajustada cores do arquivo do tema

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -3,12 +3,15 @@ export default {
 		background_900: "#0D0C0F",
 		background_800: "#1C1B1E",
 		background_700: "#262529 ",
-		background_600: "rgba(255, 133, 155, 0.05)",
+		background_600: "#312E38",
 
 		white: "#F4EDE8",
-		pink: "#FF859B",
 
+		pink: "#FF859B",
+		pink_light: "rgba(255, 133, 155, 0.05)",
+
+		gray_300: "#666360",
+		gray_200: "#3E3B47",
 		gray_100: "#948F99",
-		gray_300: "#666360",     
 	}
 };              
---
+++ src/view/Details/styles.js
@@ -0,0 +1,113 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+	width: 100%;
+	display: grid;
+	grid-template-rows: 1fr auto;
+	grid-template-areas:
+		'header'
+		'content';
+	gap: 40px;
+
+	> main {
+		width: 100%;
+		padding: 0 124px;
+		grid-area: content;
+
+		.wrapper {
+			height: 70vh;
+			padding-right: 16px;
+			overflow-y: auto;
+
+			&::-webkit-scrollbar {
+				width: 8px;
+			}
+
+			&::-webkit-scrollbar-thumb {
+				background-color: ${({ theme }) => theme.colors.pink};
+				border-radius: 9999px;
+			}
+		}
+	}
+
+	@media (max-width: 768px) {
+		main {
+			padding: 0 40px;
+		}
+
+		.wrapper {
+			height: 90vh !important;
+		}
+	}
+`
+
+export const MovieTitle = styled.div`
+	max-width: 460px;
+	display: flex;
+	flex-direction: column;
+	gap: 24px;
+
+	margin-top: 24px;
+
+	> .titleRating {
+		display: flex;
+		align-items: center;
+		gap: 10px;
+
+		h1 {
+			margin-right: 8px;
+		}
+
+		svg {
+			fill: ${({ theme }) => theme.colors.pink};
+			stroke: ${({ theme }) => theme.colors.pink};
+
+			width: 20px;
+			height: 20px;
+			margin-top: 8px;
+		}
+	}
+
+	> .autorLine {
+		display: flex;
+		align-items: center;
+		gap: 18px;
+		font: normal 400 16px 'Roboto', sans-serif;
+
+		.autor {
+			display: flex;
+			align-items: center;
+			gap: 8px;
+
+			img {
+				width: 20px;
+				height: 20px;
+				border-radius: 50%;
+			}
+		}
+
+		.time {
+			display: flex;
+			align-items: center;
+			gap: 8px;
+
+			svg {
+				width: 20px;
+				stroke: ${({ theme }) => theme.colors.pink};
+			}
+		}
+	}
+
+	> .tagsLine {
+		margin-top: 16px;
+	}
+`
+export const Content = styled.div`
+	width: 100%;
+	margin-top: 40px;
+	display: flex;
+	flex-direction: column;
+	gap: 16px;
+
+	text-align: justify;
+`
